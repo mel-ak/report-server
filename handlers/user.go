59c9177a55d299ba43182cf170b591d3abec8e49
mel-ak <melakesisay@gmail.com>
2024-12-28 10:01:37 +0300

    user handler created

---
+++ handlers/user.go
@@ -0,0 +1,66 @@
+package handlers
+
+import (
+    "time"
+
+	"github.com/mel-ak/report-server/database"
+	"github.com/mel-ak/report-server/models"
+    "github.com/gofiber/fiber/v2"
+    "golang.org/x/crypto/bcrypt"
+    "github.com/golang-jwt/jwt/v4"
+)
+
+// JWT secret key
+var jwtSecret = "your_jwt_secret_key"
+
+func RegisterUser(c *fiber.Ctx) error {
+    user := new(models.User)
+    if err := c.BodyParser(user); err != nil {
+        return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+    }
+
+    hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
+    if err != nil {
+        return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to hash password"})
+    }
+    user.Password = string(hashedPassword)
+
+    if result := database.DB.Create(&user); result.Error != nil {
+        return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": result.Error.Error()})
+    }
+
+    return c.Status(fiber.StatusCreated).JSON(user)
+}
+
+func LoginUser(c *fiber.Ctx) error {
+    var input struct {
+        Email    string `json:"email"`
+        Password string `json:"password"`
+    }
+
+    if err := c.BodyParser(&input); err != nil {
+        return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+    }
+
+    var user models.User
+    if result := database.DB.Where("email = ?", input.Email).First(&user); result.Error != nil {
+        return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Invalid credentials"})
+    }
+
+    if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(input.Password)); err != nil {
+        return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Invalid credentials"})
+    }
+
+    token := jwt.New(jwt.SigningMethodHS256)
+    claims := token.Claims.(jwt.MapClaims)
+    claims["id"] = user.ID
+    claims["email"] = user.Email
+    claims["exp"] = time.Now().Add(time.Hour * 24).Unix() // Token expires in 24 hours
+
+    t, err := token.SignedString([]byte(jwtSecret))
+    if err != nil {
+        return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to create token"})
+    }
+
+    return c.JSON(fiber.Map{"token": t})
+}
